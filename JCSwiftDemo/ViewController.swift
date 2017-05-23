9e90af08c67bfd01fff3fe9601eac60e276474f5
prajakta <prajakta@iternia.com>
2017-05-23 16:58:09 +0530

    Initial Commit

--- JCSwiftDemo/ViewController.swift
+++ JCSwiftDemo/ViewController.swift
@@ -13,6 +13,52 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("Hello Swift")
+        let name = "Hello"
+        let str = "Swift"
+        
+        let string = name + str
+        print(string)
+        
+        
+        
+        let string1 = "Hello"
+        let string2 = "Hello"
+        
+        //String Check..
+        if string1 == string2 {
+            
+            print("String are same")
+        } else {
+            print("String are not same")
+        }
+        // Array
+        var myArray = ["Jivan","Jay"]
+        print(myArray)
+        
+        //Add element in Array.
+        myArray += ["Ram","Akshay"]
+        print(myArray)
+        
+        let numberOfItems = myArray.count;
+        
+        print(numberOfItems)
+        
+        let items = myArray[1]
+        
+        myArray[1] = "Ajay"
+        print(myArray)
+        
+        //Dic..
+        var myDictionary: Dictionary <String, String> = ["APPLE" : "Apple", "ORANGE" : "orange"];
+        print(myDictionary)
+        
+        for (stockCode,name) in myDictionary {
+            print("\(stockCode) = \(name)")
+        }
+        //Add item in dic..
+        myDictionary["BANANA"] = "Banana"
+        print(myDictionary)
     }
 
     override func didReceiveMemoryWarning() {
